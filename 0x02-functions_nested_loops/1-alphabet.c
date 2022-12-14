e2310343061d567fc40cad3b0969d0a8bd7f0ed2
Ezekielsam <connect2gracealone@gmail.com>
2022-12-14 15:46:03 -0800

    trailing whitespace removed

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,13 @@
 /**
  * print_alphabet - print all alphabet in lowercase
  */
+
 void print_alphabet(void)
 {
 	char letter;
 
-	for (letter = "a"; letter <= "z"; letter++)
+	for (letter = "a"; letter <= ii"z"; letter++)
 		_putchar(letter);
-	
+
 	_putchar("\n");
 }
