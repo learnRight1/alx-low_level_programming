c26b29d3b05e6ed8d3532a362be8bcc15bab3394
Ezekielsam <connect2gracealone@gmail.com>
2023-01-01 10:58:40 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,17 @@
 /**
  * _isupper - checks if parameter is an uppercase character.
  * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * Return: give 1  or 0
  */
 int _isupper(int c);
 {
 
-	if (c >= 'A' && c <= 'Z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
