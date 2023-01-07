b434f6b3430e86a4371b89925bb273a2107f2295
Ezekielsam <connect2gracealone@gmail.com>
2023-01-06 19:46:12 -0800

    my task

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+void _puts(char *str)
+#include <stdio.h>
+/**
+* main - Entry point
+* @str: string value
+
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
+
