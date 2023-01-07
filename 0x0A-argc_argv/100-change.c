502ca44df9ee25427e186075396f766d784aab4b
Ezekielsam <connect2gracealone@gmail.com>
2023-01-06 16:44:12 -0800

    hot fix

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - prints coints left
- * @argc: parameter entry
- * @argv: one number og arguments
- * Return: 0;
+ * main - function
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0;
  */
 int main(int argc, char *argv[])
 {
-int c, coins = 0;
+int position, total, change, aux;
+int coins[] = {25, 10, 5, 2, 1};
+position = total = change = aux = 0;
 if (argc != 2)
 {
 printf("Error\n")
 return (1);
 }
-c = atoi(argv[1]);
-if (c < 0)
+total = atoi(argv[1]);
+if (total <= 0)
 {
 printf("0\n");
 return (0);
 }
-for (; c >= 0;)
+while (coins[position] != '\0')
 {
-if (c >= 25)
-c -= 25;
-else if (c >= 10)
-c -= 10;
-else if (c >= 5)
-c -= 5;
-else if (c >= 2)
-c -= 2;
-else if (c >= 1)
-c -= 1;
-else
-break;
-coins += 1;
+if (total >= coins[position])
+aux = (total / coins[position]);
+change += aux;
+total -= coins[position] * aux;
 }
-printf("%d\n", coins);
+position++;
+}
+printf("%d\n", change);
 return (0);
 }
