58cc1ac75d185b6bcf8762c63ddfd128ae92b736
Ezekielsam <connect2gracealone@gmail.com>
2023-01-11 14:24:53 -0800

    correct task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,12 +8,27 @@
  */
 char *create_array(unsigned int size, char c)
 {
-char *array = malloc(size);
-if (size == 0 || array == 0)
+char *buffer;
+unsigned int position;
+if (size == 0)
+{
 return (NULL);
-while (size--)
-array[size] = c;
-return (array);
+}
+buffer = (char *) malloc(size * sizeof(c));
+if (buffer == 0)
+{
+return (NULL);
+}
+else
+{
+position = 0;
+while (position < size)
+{
+*(buffer + position) = c;
+position++;
+}
+return (buffer);
+}
 }
 
 
