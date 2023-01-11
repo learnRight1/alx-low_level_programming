abd9b1a6e68608d0a35f5c35703da6f5fef8b68f
Ezekielsam <connect2gracealone@gmail.com>
2023-01-11 12:56:49 -0800

    update

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
@@ -9,6 +11,5 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 char **strtow(char *str);
-
 #endif
 
