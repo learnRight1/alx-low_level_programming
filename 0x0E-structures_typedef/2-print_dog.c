29acfa77fdcc62765d5e0d2006932df8ea2b9a1d
Ezekielsam <connect2gracealone@gmail.com>
2023-01-16 16:23:06 -0800

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,14 +16,14 @@ void print_dog(struct dog *d)
 	}
 	else
 	{
-		if (d-name == NULL)
+		if (d - name == NULL)
 			printf("Name: (nil)\n");
 		else
-			printf("Name: %s\n", d-name);
-		printf("Age: %f\n, d-age);
-		if (d-owner == NULL)
-			printf("owner: (nil)\n");
+			printf("Name: %s\n", d - name);
+		printf("Age: %f\n, d - age);
+		if (d - owner == NULL)
+			printf("owner : (nil)\n");
 		else
-			printf("owner: %s\n", d-owner);
+			printf("owner : % s\n", d - owner);
 	}
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -10,7 +10,7 @@ void free_dog(dog_t *d)
 {
 	if (d == 0)
 		return;
-	free(d-name);
-	free(d-owner);
+	free(d - name);
+	free(d - owner);
 	free(d);
 }
