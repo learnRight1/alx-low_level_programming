b428cb1c5a6100ee37a1e397f0309a442cb6e140
Ezekielsam <connect2gracealone@gmail.com>
2023-03-19 11:34:04 -0700

    change

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,14 @@
 #include "lists.h"
 
 /**
-<<<<<<< HEAD
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: pointer to the head of the list
- *    
+ *
  * Return: number of nodes
-=======
  * print_dlistint - Prints all the elements of a dlistint_t list.
  * @h: A pointer to the head of the list.
  *
  * Return: The number of nodes in the list.
->>>>>>> f03df6b7b07ccb6addaf19383329eeb36065a816
  */
 
 size_t print_dlistint(const dlistint_t *h)
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,17 +1,14 @@
 #include "lists.h"
 
 /**
-<<<<<<< HEAD
  * dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: pointer to the head of the list
- *    
- * Return: number of nodes
-=======
+ *
+ * Return: number of node
  * dlistint_len - Returns the number of elements in a dlistint_t list.
  * @h: A pointer to the head of the list.
  *
  * Return: The number of elements in the list.
->>>>>>> f03df6b7b07ccb6addaf19383329eeb36065a816
  */
 
 size_t dlistint_len(const dlistint_t *h)
