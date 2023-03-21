fffee518f9ebcf64a9aeeb8eb8db8444c5741ff7
Ezekielsam <connect2gracealone@gmail.com>
2023-03-21 23:03:04 +0100

    updated all the tasks

--- 0x18-dynamic_libraries/0-isupper.c
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: give 1  or 0
+ * _isupper - checks for uppercase character
+ * @c: variable text
+ * Return: Always 0
  */
-int _isupper(int c);
+int _isupper(int c)
 {
-
 if (c >= 'A' && c <= 'Z')
 {
 return (1);
@@ -17,3 +15,4 @@ else
 return (0);
 }
 }
+
--- 0x18-dynamic_libraries/4-isalpha.c
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -1,15 +1,13 @@
 #include "main.h"
 /**
- * _isalpha - checks for char alphabet
- * @c: the char to be checked
- *
- * Return: 1 if char letter is lower or upper, 0 otherwise
+ * _isalpha - checks for alphabet character
+ * @c: character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
-
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
-		return (1);
-	else
-		return (0);
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+return (1);
+else
+return (0);
 }
